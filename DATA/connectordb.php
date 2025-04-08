d21d198200e6f25dacedd89a5adcc4e17e8893fa
SONLEE2005 <sonsomot123@gmail.com>
2025-04-08 16:39:04 +0700

    connectordb.php

---
+++ DATA/connectordb.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$dbname = 'WatchStore';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Thiết lập chế độ lỗi
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Kết nối thành công!";
+} catch (PDOException $e) {
+    echo "Kết nối thất bại: " . $e->getMessage();
+}
+?>
+
