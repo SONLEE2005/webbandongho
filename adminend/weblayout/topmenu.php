d8552fc4be2b8b2e98bc199473f5cbebfcfa4736
mystictech0420 <167068656+mystictech0420@users.noreply.github.com>
2025-04-30 19:09:16 +0700

    Create topmenu.php

---
+++ adminend/weblayout/topmenu.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="header">
+            <div>
+                <h1>Logo here*</h1>
+                <h1>Welcome to the Admin End</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><button id="profile">Pfp</button></li>
+                    <li><button id="login">Login</button></li>
+                    <li><button id="signup">Sign up</button></li>
+                    <li><button id="cart">Cart</button></li>
+                </ul>
+            </nav>
+    </div>
+    <script src="../button.js"></script>
+</body>
+</html>
