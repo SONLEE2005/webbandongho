b9a58df3235668da6d32056ca85396d5db029033
mystictech0420 <167068656+mystictech0420@users.noreply.github.com>
2025-04-30 19:09:56 +0700

    Create footer.php

---
+++ adminend/weblayout/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="footer">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+        <p>Privacy Policy | Terms of Service</p>
+        <p>Follow us on: 
+            <a href="#">Facebook</a> | 
+            <a href="#">Twitter</a> | 
+            <a href="#">Instagram</a>
+        </p>    
+    </div>
+</body>
+</html>
